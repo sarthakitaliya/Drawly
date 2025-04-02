15d550ca3f41dd19f86af958f7afd46ac073365c
Sarthak Italiya <sarthakitaliya@Sarthaks-MacBook-Pro.local>
2025-04-02 21:37:20 +0530

    fixing: production cookie issue

--- apps/web/app/api/auth/[...nextauth]/option.ts
+++ apps/web/app/api/auth/[...nextauth]/option.ts
@@ -12,18 +12,31 @@ export const authOption: NextAuthOptions = {
     }),
   ],
   session: {
-    strategy: "jwt", 
-    maxAge: 7 * 24 * 60 * 60, 
+    strategy: "jwt",
+    maxAge: 7 * 24 * 60 * 60,
   },
-  jwt:{
-    encode: async({token, secret}) => {
+  useSecureCookies: true,
+  cookies: {
+    sessionToken: {
+      name: "__Secure-next-auth.session-token",
+      options: {
+        httpOnly: true,
+        secure: true, 
+        sameSite: "none", 
+        domain: ".sarthak-dev.me", 
+        path: "/",
+      },
+    },
+  },
+  jwt: {
+    encode: async ({ token, secret }) => {
       //@ts-ignore
-      return jwt.sign(token, secret, {algorithm: "HS256"});
+      return jwt.sign(token, secret, { algorithm: "HS256" });
     },
     decode: (params) => {
       //@ts-ignore
       return jwt.verify(params.token, params.secret) as JWT;
-    }
+    },
   },
   callbacks: {
     async jwt({ token, user, account }) {
