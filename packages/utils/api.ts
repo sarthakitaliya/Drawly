d398c8f75737baf40da5285ed1c1b316bf756a31
Sarthak Italiya <sarthakitaliya@Sarthaks-MacBook-Pro.local>
2025-04-02 23:15:56 +0530

    fixing: production cookie issue

--- packages/utils/api.ts
+++ packages/utils/api.ts
@@ -3,5 +3,8 @@ import { API_URL } from "./config"
 
 export const api = axios.create({
     baseURL: API_URL || "http://localhost:3001/api",
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
 })
